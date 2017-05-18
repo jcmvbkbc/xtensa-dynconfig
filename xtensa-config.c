a63df64eaeb85d2df098475b66924507221d9390
Max Filippov <jcmvbkbc@gmail.com>
2017-05-17 18:13:36 -0700

    add builtin gcc __XCHAL_* macro definitions
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- xtensa-config.c
+++ xtensa-config.c
@@ -1,5 +1,18 @@
+#include <stdlib.h>
 #define XTENSA_CONFIG_DEFINITION
 #include <xtensa-config.h>
 #include <xtensa-dynconfig.h>
 
+#undef XTENSA_CONFIG_ENTRY
+#define XTENSA_CONFIG_ENTRY(a) a
 struct xtensa_config xtensa_config = XTENSA_CONFIG_INITIALIZER;
+
+#define _STRINGIFY(a) #a
+#define STRINGIFY(a) _STRINGIFY(a)
+
+#undef XTENSA_CONFIG_ENTRY
+#define XTENSA_CONFIG_ENTRY(a) "__" #a "=" STRINGIFY(a)
+const char *xtensa_config_strings[] = {
+	XTENSA_CONFIG_ENTRY_LIST,
+	NULL,
+};
